e306c5a53888302330704834d460dbee11c8cce6
Neo40 <tron36.cc@gmail.com>
2023-11-16 19:33:40 -0300

    Entrada de numero via teclado

---
+++ src/desafios/NumeroReverso.java
@@ -0,0 +1,14 @@
+package desafios;
+
+import java.util.Scanner;
+
+public class NumeroReverso {
+    public static void numeroReveso() {
+        Scanner scan = new Scanner(System.in);
+        do {
+            System.out.println("Informe um valor inteiro e positivo (este numero deverá conter 4 dígitos):");
+        } while (true);
+
+    }
+}
+
