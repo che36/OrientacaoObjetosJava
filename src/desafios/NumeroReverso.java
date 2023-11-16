19ac8c6a159a2197a095170e4c375897576c9173
Neo40 <tron36.cc@gmail.com>
2023-11-16 19:48:37 -0300

    Transformação de um numero em String

--- src/desafios/NumeroReverso.java
+++ src/desafios/NumeroReverso.java
@@ -7,8 +7,13 @@ public class NumeroReverso {
         Scanner scan = new Scanner(System.in);
         do {
             System.out.println("Informe um valor inteiro e positivo (este numero deverá conter 4 dígitos):");
+            int numeroInformado = scan.nextInt();
+            String numeroInformadoString = String.valueOf(numeroInformado);
+            if (numeroInformado >= 1000 && numeroInformado <= 9999) {
+                StringBuilder stringBuilder = new StringBuilder(numeroInformadoString);
+                System.out.println();
+            }
         } while (true);
-
     }
 }
 
