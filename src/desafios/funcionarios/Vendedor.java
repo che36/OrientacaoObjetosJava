6224d7f5610199790cb59d243a64a15d42fd6eab
Neo40 <tron36.cc@gmail.com>
2023-11-18 14:07:13 -0300

    Classe vendedor com implementacao abstrata

---
+++ src/desafios/funcionarios/Vendedor.java
@@ -0,0 +1,34 @@
+package desafios.funcionarios;
+
+public class Vendedor extends Funcionario {
+    private double comissao;
+    private long numeroVendas;
+
+    public Vendedor(String nome, long cpf, double comissao, long numeroVendas) {
+        super(nome, cpf);
+        this.comissao = comissao;
+        this.numeroVendas = numeroVendas;
+    }
+
+
+    @Override
+    public double calcularSalario() {
+        return super.getSALARIO_MININO() + this.comissao * this.numeroVendas;
+    }
+
+    public double getComissao() {
+        return comissao;
+    }
+
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+
+    public long getNumeroVendas() {
+        return numeroVendas;
+    }
+
+    public void setNumeroVendas(long numeroVendas) {
+        this.numeroVendas = numeroVendas;
+    }
+}
