c0433e94a4c0c58ecc12eb8b6956548f95cc6c59
Neo40 <tron36.cc@gmail.com>
2023-11-18 14:24:25 -0300

    Adição de 2 objetos

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,26 @@
-import desafios.Macaco;
+import desafios.funcionarios.Consultor;
+import desafios.funcionarios.Vendedor;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
         //NumeroReverso.numeroReveso();
-        Macaco macaco1 = new Macaco();
+        //Macaco macaco1 = new Macaco();
 
-        macaco1.comer("banana");
-        macaco1.comer("uva");
-        macaco1.comer("morango");
+        //macaco1.comer("banana");
+        //macaco1.comer("uva");
+        //macaco1.comer("morango");
 
 
-        macaco1.digerir();
-        macaco1.digerir();
-        macaco1.digerir();
-        macaco1.digerir();
+        //macaco1.digerir();
+        //macaco1.digerir();
+        //macaco1.digerir();
+        //macaco1.digerir();
 
+        Vendedor vendedor = new Vendedor("Cesar Vendedor", 123456l, 14d, 45l);
+
+        Consultor consultor = new Consultor("Consultor", 123456l, 45d, 84);
     }
 
 }
